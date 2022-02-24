0c4af6828fd7c96817c83fc5babecb83000b465c
nig <nig@tutao.de>
2022-02-24 11:06:05 +0100

    [desktop] add SafeStorageSecretStorage SecretStorage impl
    uses the electron.safeStorage API to store secrects instead of
    depending on the native keytar module

--- src/api/common/error/DeviceStorageUnavailableError.ts
+++ src/api/common/error/DeviceStorageUnavailableError.ts
@@ -1,9 +1,13 @@
 //@bundleInto:common-min
 
-import {TutanotaError} from "./TutanotaError"
+import {TutanotaError} from "./TutanotaError.js"
 
 export class DeviceStorageUnavailableError extends TutanotaError {
-	constructor(msg: string, error: ?Error) {
-		super("DeviceStorageUnavailableError", error ? (msg + "> " + (error.stack ? error.stack : error.message)) : msg)
+
+	constructor(msg: string, error: Error | null) {
+		const message = error
+			? (msg + "> " + (error.stack ? error.stack : error.message))
+			: msg
+		super("DeviceStorageUnavailableError", message)
 	}
 }
